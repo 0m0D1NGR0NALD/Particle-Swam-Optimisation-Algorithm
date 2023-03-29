070e8d2fb0f498959428d152b2b085589e556999
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-29 11:19:02 +0300

    Update particle.py

--- particle.py
+++ particle.py
@@ -8,3 +8,7 @@ def f(x,y):
 # Compute and plot the function in 3D within [0,5]x[0,5]
 x, y = np.array(np.meshgrid(np.linspace(0,5,1000), np.linspace(0,5,1000)))
 z = f(x, y)
+
+# Find the global minimum
+x_min = x.ravel()[z.argmin()]
+y_min = y.ravel()[z.argmin()]
