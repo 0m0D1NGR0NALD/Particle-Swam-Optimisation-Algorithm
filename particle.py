76df28ae8f8ff2f7da73c34ac9c4f30eaffbcd43
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-29 11:18:09 +0300

    Update particle.py

--- particle.py
+++ particle.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 def f(x,y):
     "Objective function"
     return (x-2)**2 + (y-2)**2 + np.sin(5*x+2) + np.sin(2*y-3)
+    
+# Compute and plot the function in 3D within [0,5]x[0,5]
+x, y = np.array(np.meshgrid(np.linspace(0,5,1000), np.linspace(0,5,1000)))
+z = f(x, y)
