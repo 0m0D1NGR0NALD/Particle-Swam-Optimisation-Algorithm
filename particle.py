9a8e5920c14bcfc887dd991213cb23c464c06e8c
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-30 15:24:56 +0300

    Update particle.py

--- particle.py
+++ particle.py
@@ -31,13 +31,15 @@ gbest = pbest[:, pbest_obj.argmin()]
 gbest_obj = pbest_obj.min()
 
 # Generating PSO algorithm
-
-# Update params
-r1, r2 = np.random.rand(2)
-V = w * V + c1*r1*(pbest - X) + c2*r2*(gbest.reshape(-1,1)-X)
-X = X + V
-obj = f(X[0], X[1])
-pbest[:, (pbest_obj >= obj)] = X[:, (pbest_obj >= obj)]
-pbest_obj = np.array([pbest_obj, obj]).min(axis=0)
-gbest = pbest[:, pbest_obj.argmin()]
-gbest_obj = pbest_obj.min()
+def update():
+    "Function to do one iteration of particle swarm optimization"
+    global V, X, pbest, pbest_obj, gbest, gbest_obj
+    # Update params
+    r1, r2 = np.random.rand(2)
+    V = w * V + c1*r1*(pbest - X) + c2*r2*(gbest.reshape(-1,1)-X)
+    X = X + V
+    obj = f(X[0], X[1])
+    pbest[:, (pbest_obj >= obj)] = X[:, (pbest_obj >= obj)]
+    pbest_obj = np.array([pbest_obj, obj]).min(axis=0)
+    gbest = pbest[:, pbest_obj.argmin()]
+    gbest_obj = pbest_obj.min()
